b1c33e0aeaa07ec9326dec788e5e7b3d7770c9a9
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-11-11 23:23:51 +0530

    Add uc 3

---
+++ AddressBookSystem4/AddressBookSystem4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace AddressBookSystem4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of contact entries you want to have: ");
+            int num = int.Parse(Console.ReadLine());
+
+            ContactPerson obj = new ContactPerson();
+            for (int i = 0; i < num; i++)
+            {
+
+                obj.AddContact();
+            }
+            Console.WriteLine("Do you want to see Addressbook entries(y/Y) :");
+            char ch = char.Parse(Console.ReadLine());
+            if (ch == 'y' || ch == 'Y')
+            {
+                obj.displayContact();
+            }
+
+            Console.WriteLine("You Want to update(y/n) :");
+            char ch11 = char.Parse(Console.ReadLine());
+            if (ch11 == 'y')
+            {
+                Console.WriteLine("Enter name You Want to update :");
+                string str1 = Console.ReadLine();
+                obj.updateContact(str1);
+            }
+
+
+            Console.WriteLine("Do you want to see Addressbook entries(y/Y) :");
+            char ch2 = char.Parse(Console.ReadLine());
+            if (ch2 == 'y' || ch2 == 'Y')
+            {
+                obj.displayContact();
+            }
+        }
+    }
+}
